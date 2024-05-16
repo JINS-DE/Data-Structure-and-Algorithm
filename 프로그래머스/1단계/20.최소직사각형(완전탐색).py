3553e5a2113036f6b2554f1d732ed728d54ac1d7
JINS-DE <rlawlstjd044@naver.com>
2024-05-16 23:58:40 +0900

    first commit

---
+++ 프로그래머스/1단계/20.최소직사각형(완전탐색).py
@@ -0,0 +1,16 @@
+def solution(sizes):
+    answer = 0
+    return answer
+
+sizes=[[60, 50], [30, 70], [60, 30], [80, 40],[10,10]]
+w1=0
+h1=0
+w2=0
+h2=0
+
+for i in sizes:
+    w1=i[0]
+    h1=i[1]
+    
+    
+
