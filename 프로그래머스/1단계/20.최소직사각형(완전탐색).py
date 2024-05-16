56d89636cdac85c332320d9265b14bf9bd2d9c81
JINS-DE <rlawlstjd044@naver.com>
2024-05-17 00:18:45 +0900

    answer

--- 프로그래머스/1단계/20.최소직사각형(완전탐색).py
+++ 프로그래머스/1단계/20.최소직사각형(완전탐색).py
@@ -1,16 +1,12 @@
 def solution(sizes):
-    answer = 0
-    return answer
+    w1=0
+    h1=0
+    w2=0
+    h2=0
 
-sizes=[[60, 50], [30, 70], [60, 30], [80, 40],[10,10]]
-w1=0
-h1=0
-w2=0
-h2=0
-
-for i in sizes:
-    w1=i[0]
-    h1=i[1]
-    
-    
-
+    for i in sizes:
+        w2=i[0]
+        h2=i[1]
+        w1=max(max(w1,h1),max(w2,h2))
+        h1=max(min(w1,h1),min(w2,h2))
+    return w1*h1
