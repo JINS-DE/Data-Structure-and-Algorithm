2678105ea2964a376e5c2e0a72f5aa6c0e6b6ff4
Deon <115610267+JINS-DE@users.noreply.github.com>
2024-08-22 19:13:04 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,5 +1,13 @@
-n=int(input())
-if n%2==0:
-    print('CY')
-else:
-    print('SK')
+# DP로 해결시
+N = int(input())
+dp = [0]*1001
+
+# SK == 0, CY == 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 0
+
+for i in range(4, N+1):
+    dp[i] = (dp[i-1] + 1) % 2  # 또는 dp[i] = (dp[i-3] + 1) % 2
+
+print('SK' if dp[N] == 0 else 'CY')
