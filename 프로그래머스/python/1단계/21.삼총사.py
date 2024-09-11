c51e34fcd42686c2a8be7520d3bb94c48c1097eb
Deon <115610267+JINS-DE@users.noreply.github.com>
2024-09-11 18:29:27 +0900

    Delete 프로그래머스 directory

--- 프로그래머스/python/1단계/21.삼총사.py
+++
@@ -1,9 +0,0 @@
-number=	[-3, -2, -1, 0, 1, 2, 3]
-
-answer = 0
-for i in range(len(number)-2):
-    for j in range(i+1,len(number)-1):
-        for k in range(j+1,len(number)):
-            if number[i]+number[j]+number[k] == 0:
-                answer+=1
-print(answer)
--- 프로그래머스/sql/1단계/특정 형질을 찾는 대장균 찾기.sql
+++
@@ -1,3 +0,0 @@
--- 비트 연산으로 문제를 풀자
--- 이진수 1111 일 때 2번째는 0이여야 하고 첫 번째나 세 번째가 1이면 COUNT 하는 문제다.
-SELECT COUNT(ID) AS COUNT FROM ECOLI_DATA WHERE (GENOTYPE & 2 = 0) AND (GENOTYPE & 1>0 OR GENOTYPE & 4 > 0)
