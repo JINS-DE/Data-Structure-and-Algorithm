91aeebb82b3cb85fb65db25bc076585acb76c0a3
Deon <115610267+JINS-DE@users.noreply.github.com>
2024-10-27 22:51:06 +0900

    [Bronze II] Title: 피보나치 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,20 +1,18 @@
 import sys
-n=sys.stdin.readline()
-dp = {}
-def fibonacci_top_down(n):
-    # 기본 조건: F(0)과 F(1)
-    if n == 0:
+n = sys.stdin.readline()
+dp={}
+
+def pibo(n):
+    if n==0:
         return 0
-    if n == 1:
+    if n==1:
         return 1
     
-    # 이미 계산된 값이 있다면 그 값을 사용
     if n in dp:
         return dp[n]
 
-    # 값을 계산하고 dp에 저장
-    dp[n] = fibonacci_top_down(n - 1) + fibonacci_top_down(n - 2)
+    dp[n] = pibo(n-1)+pibo(n-2)
     return dp[n]
+    
 
-# 예시: F(5)를 구할 때
-print(fibonacci_top_down(int(n)))  # 결과: 5
+print(pibo(int(n)))
