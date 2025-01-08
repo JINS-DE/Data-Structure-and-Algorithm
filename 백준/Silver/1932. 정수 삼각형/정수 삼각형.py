9040288d679cbd2554a2d8490cdef23b950af4c5
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-01-09 07:44:18 +0900

    [Silver I] Title: 정수 삼각형, Time: 148 ms, Memory: 41660 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr=[]
+dp=[]
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+    dp.append([0]*(i+1))
+
+dp[0][0] = arr[0][0]
+for i in range(1,n):
+    for j in range(len(arr[i])):
+        if j == 0 :
+            dp[i][0] = dp[i-1][0] + arr[i][0]
+        elif j == len(arr[i])-1 :
+            dp[i][j] = dp[i-1][-1] + arr[i][-1]
+        else:
+            dp[i][j] = max(dp[i-1][j-1]+arr[i][j],dp[i-1][j]+arr[i][j])
+print(max(dp[-1]))
