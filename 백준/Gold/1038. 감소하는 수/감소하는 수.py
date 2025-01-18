c26cc5684d8f2ca870b6091fbd8d09d0ebd25e91
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-01-19 00:03:20 +0900

    [Gold V] Title: 감소하는 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -4,10 +4,9 @@ N = int(input())
 
 result = []
 for i in range(1, 11):
-	for j in combinations(range(10), i):
-		num = ''.join(list(map(str, reversed(list(j)))))
-		result.append(int(num))
-
+    for j in combinations(range(10), i):
+        num = ''.join(map(str, reversed(j)))
+        result.append(int(num))
 result.sort()
 if N >= len(result):
 	print(-1)
