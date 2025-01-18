179c14af0847580ac794eb12cf7be495aa1aa7f9
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-01-18 23:32:50 +0900

    [Gold V] Title: 감소하는 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -1,22 +1,15 @@
-from collections import deque
+from itertools import combinations
 
-def find_decreasing_number(n):
-    if n > 1022:  # 감소하는 수는 최대 1022개
-        return -1
+N = int(input())
 
-    queue = deque(range(10))  # 초기값: 0 ~ 9를 큐에 넣음
-    count = -1  # 순서를 셀 변수
+result = []
+for i in range(1, 11):
+	for j in combinations(range(10), i):
+		num = ''.join(list(map(str, reversed(list(j)))))
+		result.append(int(num))
 
-    while queue:
-        current = queue.popleft()
-        count += 1
-
-        if count == n:  # N번째 감소하는 수를 찾으면 반환
-            return current
-
-        last_digit = current % 10  # 현재 숫자의 마지막 자릿수
-        for next_digit in range(last_digit):  # 감소하는 숫자를 만듦
-            queue.append(current * 10 + next_digit)
-
-n = int(input())
-print(find_decreasing_number(n))
+result.sort()
+if N >= len(result):
+	print(-1)
+else:
+	print(result[N])
