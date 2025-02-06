799b558e47cce088d81b7e97c7dba217fa8b7c8e
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-02-06 15:21:59 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.44 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,15 +1,30 @@
 def solution(bandage, health, attacks):
+    last_attacked_time = attacks[-1][0]
+    attacks_dic = dict()
+    for time, damage in attacks:
+        attacks_dic[time]=damage
+    time=0
+    bandage_stack=0
     hp=health
-    b_a = 0 # before monster attack time
-    for i in attacks:
-        time_diff=i[0]-b_a-1 
-        hp+=(time_diff//bandage[0])*bandage[2] + bandage[1]*time_diff
-        if hp>=health:
+    
+    while True:
+        time+=1
+        if time>last_attacked_time or hp<0:
+            break
+        
+        if time in attacks_dic:
+            hp-=attacks_dic[time]
+            bandage_stack=0
+            continue
+        
+        bandage_stack+=1 # 초당 회복량
+        if bandage_stack == bandage[0]: # 시전 시간
+            hp+= bandage[1]+bandage[2] # 추가 회복량까지
+            bandage_stack=0
+        else:
+            hp+= bandage[1]
+
+        if hp>health:
             hp=health
-        hp-=i[1]
-        
-        if hp<=0:
-            return -1
-        
-        b_a=i[0] 
-    return hp
+
+    return hp if hp>0 else -1
