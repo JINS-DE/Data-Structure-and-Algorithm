c62c2f5c93d7682b2653ed5112780d165e328562
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-02-10 07:29:17 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.49 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,28 +1,29 @@
 def solution(friends, gifts):
-    li = [[0]*len(friends) for _ in range(len(friends))] 
-    answer_li=[0]*len(friends)
-
-    for i in gifts: # 2차원 list 생성
-        giver,receiver=i.split(' ')
-        g=friends.index(giver) # 준 친구
-        r=friends.index(receiver) # 받은 친구
-        li[r][g]=li[r][g]+1
-
-
-    present_index=[] # 선물지수!
-    for i in range(len(friends)):
-        present_index.append(sum(map(lambda x : x[i], li)) -sum(li[i]))
-
-
+    dic = dict()
+    for i,friend in enumerate(friends):
+        dic[friend] = i
+    
+    gifts_give_li = [[0]*len(friends) for _ in range(len(friends))]
+    
+    gifts_index=[0]*len(friends)
+    
+    for gift in gifts:
+        A, B = gift.split()
+        gifts_give_li[dic[A]][dic[B]]+=1
+        gifts_index[dic[A]]+=1
+        gifts_index[dic[B]]-=1
+    
+    expectedGifts=[0]*len(friends)
     for i in range(len(friends)):
         for j in range(i+1,len(friends)):
-            if li[i][j] > li[j][i]:
-                answer_li[j] += 1
-            elif li[i][j] < li[j][i]:
-                answer_li[i] += 1
+            if gifts_give_li[i][j] > gifts_give_li[j][i]:
+                expectedGifts[i]+=1
+            elif gifts_give_li[i][j] < gifts_give_li[j][i]:
+                expectedGifts[j]+=1
             else:
-                if present_index[i]>present_index[j]:
-                    answer_li[i] += 1
-                elif present_index[i]<present_index[j]:
-                    answer_li[j] += 1
-    return max(answer_li)
+                if gifts_index[i]>gifts_index[j]:
+                    expectedGifts[i]+=1
+                elif gifts_index[i]<gifts_index[j]:
+                    expectedGifts[j]+=1
+                  
+    return max(expectedGifts)
