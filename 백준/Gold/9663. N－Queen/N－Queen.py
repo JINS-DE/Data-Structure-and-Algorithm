b2f5bd47efc0df11272a8e1330960e8ccb04aaf2
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-02-14 20:59:55 +0900

    [Gold IV] Title: N-Queen, Time: 29808 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,18 +1,18 @@
-n=int(input())
-answer=0
-v1=[0]*n
-v2=[0]*(2*n)
-v3=[0]*(2*n)
-
-def N_Queen(row):
+def Nqueen(row):
     global answer
-    if row == n:
+    if N==row:
         answer+=1
         return
-    for col in range(n):
-        if v1[col]== v2[row+col] == v3[row-col]==0:
-            v1[col]= v2[row+col]=v3[row-col]=1
-            N_Queen(row+1)
-            v1[col]= v2[row+col]=v3[row-col]=0
-N_Queen(0)
+    
+    for col in range(N):
+        if v1[col] == v2[row+col] == v3[row-col]==0:
+            v1[col]= v2[row+col]= v3[row-col]=1
+            Nqueen(row+1)
+            v1[col]= v2[row+col]= v3[row-col]=0
+answer=0
+N=int(input())
+v1 = [0]*N
+v2 = [0]*(2*N)
+v3 = [0]*(2*N)
+Nqueen(0)
 print(answer)
