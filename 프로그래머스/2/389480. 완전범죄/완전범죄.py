35b80866bafa3128d42f903716f28aa512dd7322
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-02-26 16:15:46 +0900

    [level 2] Title: 완전범죄, Time: 0.75 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,30 +1,16 @@
 def solution(info, n, m):
-    length = len(info)
-    # dp[i][a][b] : info[i] 사용했을 때 a의 누적 흔적, b의 누적 흔적
-    dp=[[[False]*m for _ in range(n)] for _ in range(length+1)]
-    dp[0][0][0] = True
-    
-    for i in range(length):
-        a_cost, b_cost = info[i]
-        for a in range(n):
-            for b in range(m):
-                if dp[i][a][b]:
-                    # a가 훔칠 때
-                    na=a+a_cost
-                    nb=b
-                    if na<n:
-                        dp[i+1][na][nb]=True
-                    
-                    # b가 훔칠 때
-                    na=a
-                    nb=b+b_cost
-                    if nb<m:
-                        dp[i+1][na][nb]=True
-
-    answer=-1
-    for a in range(n):
-        for b in range(m):
-            if dp[length][a][b]:
-                if answer==-1 or a<answer:
-                    answer=a
-    return answer
+    dic = {0:0}
+    for a,b in info:
+        tmp = {}
+        for aa, bb in dic.items():
+            if aa+a<n:
+                if aa+a not in tmp or tmp[aa+a] > bb:
+                    tmp[aa+a]=bb
+            if bb+b<m:
+                if aa not in tmp or tmp[aa] > bb+b:
+                    tmp[aa]=bb+b
+        if tmp:
+            dic=tmp
+        else:
+            return -1
+    return min(dic.keys())
