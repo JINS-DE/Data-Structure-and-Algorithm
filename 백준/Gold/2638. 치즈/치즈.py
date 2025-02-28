5a9ce01dc3b83e5b356539120da51a5757e29a1f
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-02-28 16:38:03 +0900

    [Gold III] Title: 치즈, Time: 372 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,45 +1,62 @@
-def bfs(x,y):
-    q = deque([(x,y)])
-    visited[x][y]=1
+def bfs():
+    global n,m
+    flag=True
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]==1:
+                flag = False
+                air_cnt=0
+                for k in range(4):
+                    nx=dx[k]+i
+                    ny=dy[k]+j
+               
+                    if 0<=nx<n and 0<=ny<m and graph[nx][ny]==-1:
+                        air_cnt+=1
 
+                    if air_cnt>=2:
+                        queue.append((i,j))
+                        break
+    return flag
+
+def aircheck(a,b):
+    q.append((a,b))
+    graph[a][b]=-1
     while q:
-        x,y = q.popleft()
+        x,y=q.popleft()
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0<=nx<n and 0<=ny<m:
-                
-                if visited[nx][ny]==0 and graph[nx][ny]==0:
-                    visited[nx][ny]=1
-                    q.append((nx,ny))
-                elif graph[nx][ny]==1:
-                    visited[nx][ny]+=1
-
+            nx=dx[i]+x
+            ny=dy[i]+y
+            
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
+                graph[nx][ny]=-1
+                q.append((nx,ny))
 
 import sys
 from collections import deque
+input=sys.stdin.readline
 n,m = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-time = 0
+queue=deque()
+q=deque()
+answer=0
+
+aircheck(0,0)
+
 while True:
-    visited=[[0 for _ in range(m)] for _ in range(n)]
-    bfs(0,0)
-    time += 1
-
-    for i in range(n):
-        for j in range(m):
-            if visited[i][j] >= 2:
-                graph[i][j]=0
     
-    blank=0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j]==1:
-                blank+=1
-
-    if blank==0:
-        print(time)
+    if bfs():
+        print(answer)
         break
+        
+    while queue:
+        x,y=queue.popleft()
+        graph[x][y]=-1
+        for k in range(4):
+            nx=dx[k]+x
+            ny=dy[k]+y
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
+                aircheck(nx,ny)
+    answer+=1
