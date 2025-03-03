081d1f965a4b3931c917afcd16d64b09ba533ee6
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-03-04 08:37:38 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 352.39 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,16 @@
+import heapq
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    delivery = 0 
+    pickup = 0 
+    
+    for i in range(n-1, -1, -1): 
+        delivery += deliveries[i] 
+        pickup += pickups[i] 
+        
+        while delivery > 0 or pickup > 0: 
+            delivery -= cap 
+            pickup -= cap 
+            answer += (i+1) * 2 
+                 
+    return answer
