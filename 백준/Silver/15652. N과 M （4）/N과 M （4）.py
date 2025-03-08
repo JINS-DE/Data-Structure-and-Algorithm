444c34ad89ba413200ad2dfb0910c4f94e738be0
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-03-08 15:14:23 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+N,M = map(int,input().split())
+
+answer=[]
+def dfs(depth,lst,k):
+    if depth==M:
+        answer.append(lst)
+        return
+    for i in range(k,N+1):
+        dfs(depth+1,lst+[i],i)
+
+
+        
+dfs(0,[],1)
+
+for i in answer:
+    print(*i)
