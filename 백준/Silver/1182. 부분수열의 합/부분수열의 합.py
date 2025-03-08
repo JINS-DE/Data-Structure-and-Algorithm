22bb99c706360f901f5fa3e0cdbb04908452a5fb
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-03-08 13:52:28 +0900

    [Silver II] Title: 부분수열의 합, Time: 36 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,21 +1,36 @@
-import sys
-input = sys.stdin.readline
-n,m = map(int,input().split())
-arr = list(map(int,input().split()))
-answer=0
-def dfs(depth, current_sum):
-    global answer
+from itertools import combinations
+import bisect
 
-    if depth == n :
-        if current_sum == m:
-            answer+=1
-        return
-    dfs(depth+1,current_sum)
-    dfs(depth+1, current_sum+arr[depth])
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
 
-dfs(0,0)
+# 배열을 반으로 나눠서 부분합 계산
+mid = n // 2
+left_part = arr[:mid]
+right_part = arr[mid:]
 
+left_sums = []
+right_sums = []
 
+# 부분합 구하기
+for i in range(len(left_part) + 1):
+    for comb in combinations(left_part, i):
+        left_sums.append(sum(comb))
+
+for i in range(len(right_part) + 1):
+    for comb in combinations(right_part, i):
+        right_sums.append(sum(comb))
+
+# 정렬
+right_sums.sort()
+
+# 이분 탐색으로 m을 만족하는 쌍 찾기
+answer = 0
+for s in left_sums:
+    target = m - s
+    answer += bisect.bisect_right(right_sums, target) - bisect.bisect_left(right_sums, target)
+
+# 공집합 제거
 if m == 0:
     answer -= 1
 
