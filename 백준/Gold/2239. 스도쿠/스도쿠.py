36b64a72f48c8d2cc76bb750553212ba17148f0f
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-03-21 09:48:03 +0900

    [Gold IV] Title: 스도쿠, Time: 1188 ms, Memory: 116224 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,46 +1,50 @@
+# 백준 2239
+# 약 5200 ms
 import sys
 input = sys.stdin.readline
-board=[]
-for _ in range(9):
-    board.append(list(map(int,input().strip())))
+row_check = [0]*9
+col_check = [0]*9
+area_check = [0]*9
+points = []
 
-zero_li = []
+result = []
 for i in range(9):
+    row = list(map(int, input().rstrip()))
+    result.append(row)
     for j in range(9):
-        if board[i][j]==0:
-            zero_li.append((i,j))
+        num = row[j]
+        if num == 0:
+            points.append((i,j))
+        else:
+            bit = 1 << (num-1)
+            row_check[i] |= bit
+            col_check[j] |= bit
+            area_check[3 * (i//3) + (j//3)] |= bit
 
-def is_right(r,c,num):
-    for i in range(9):
-        if board[i][c]==num:
-            return False
-        if board[r][i]==num:
-            return False
-    r=(r//3)*3
-    c=(c//3)*3
-    for i in range(3):
-        for j in range(3):
-            if board[r+i][c+j]==num:
-                return False
-
-    return True
-
-def dfs(idx):
+def dfs(idx:int) -> bool:
+    if idx == len(points):
+        return True
     
-    if idx == len(zero_li):
-        for i in board:
-            print(*i,sep="")
-        sys.exit(0)
+    row, col = points[idx]
+    for num in range(9):
+        bit = 1 << num
+        if row_check[row] & bit: continue
+        if col_check[col] & bit: continue
+        if area_check[3*(row//3) + (col//3)] & bit: continue
 
-    r,c=zero_li[idx]
-
-    for i in range(1,10):
+        row_check[row] |= bit
+        col_check[col] |= bit
+        area_check[3*(row//3) + (col//3)] |= bit
+        result[row][col] = num+1
         
-        if is_right(r,c,i):
-            board[r][c]=i
-            dfs(idx+1)
-            board[r][c]=0
-            
-            
+        if dfs(idx+1):
+            return True
 
+        row_check[row] ^= bit
+        col_check[col] ^= bit
+        area_check[3*(row//3) + (col//3)] ^= bit
+        result[row][col] = 0
+    return False
+        
 dfs(0)
+for r in result:print(''.join(map(str, r)))
