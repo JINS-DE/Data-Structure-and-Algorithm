650bc5fcd333c396716a61528a6310f30c997eec
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-04-03 10:57:57 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 58.79 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,40 +1,44 @@
 def solution(m, n, board):
     answer = 0
     board = [list(b) for b in board]
-    def remove_block():
-        result = set()
-        # 4블록 제거
+    def find_4block():
+        remove_lst = set()
         for i in range(m-1):
             for j in range(n-1):
-                if board[i][j]=='':
+                if board[i][j]=="":
                     continue
                 if board[i][j]==board[i+1][j]==board[i][j+1]==board[i+1][j+1]:
-                    result.add((i,j))
-                    result.add((i+1,j))
-                    result.add((i,j+1))
-                    result.add((i+1,j+1))
-        return result
+                    remove_lst.add((i,j))
+                    remove_lst.add((i+1,j))
+                    remove_lst.add((i,j+1))
+                    remove_lst.add((i+1,j+1))
+        return remove_lst
+    
+    remove_lst = find_4block()
+    
+    while remove_lst:
+        answer+=len(remove_lst)
         
-    remove_set = remove_block()
-    while remove_set:
-        answer+=len(remove_set)
-        for i,j in remove_set:
+        # 4블록 제거
+        for i,j in remove_lst:
             board[i][j]=''
+        
+        # 블록 아래로 내리기
         for col in range(n):
-            top, buttom = m-1, m-1
-            while top>=0:
+            top,buttom=m-1,m-1
+            while top >=0:
                 if board[top][col]!='' and board[buttom][col]!='':
                     top-=1
                     buttom-=1
-                elif board[top][col]=='' and board[buttom][col]=='':
+                elif board[top][col]==board[buttom][col]=='':
                     top-=1
                 else:
-                    board[top][col], board[buttom][col] = board[buttom][col], board[top][col]
+                    board[top][col],board[buttom][col] = board[buttom][col], board[top][col]
                     top-=1
                     buttom-=1
-                
-        remove_set = remove_block()
-    
+        
+        # 다시 4블록 확인
+        remove_lst=find_4block()
     
     
     return answer
