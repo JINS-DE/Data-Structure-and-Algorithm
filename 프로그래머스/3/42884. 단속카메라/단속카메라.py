913d654e9f7ece3a7ee069c139eec77cdbb50342
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-04-22 09:44:24 +0900

    [level 3] Title: 단속카메라, Time: 2.40 ms, Memory: 9.78 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,14 @@
+def solution(routes):
+    routes.sort(key=lambda x : x[1])
+    print(routes)
+    answer=1
+    start=routes[0][0]
+    end=routes[0][1]
+    
+    for i in range(1,len(routes)):
+        if routes[i][0] <= end:
+            continue
+        start=routes[i][0]
+        end=routes[i][1]
+        answer+=1
+    return answer
