0ab4eea2ed6013f4059712cdf49b6b6057dfd206
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-04-23 14:42:38 +0900

    [level 3] Title: 이중우선순위큐, Time: 97.42 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,7 +3,7 @@ import heapq
 def solution(operations):
     min_heap = []
     max_heap = []
-    visited = [False] * len(operations)
+    visited = [0] * len(operations)
 
     for i, oper in enumerate(operations):
         command, num = oper.split()
@@ -12,28 +12,28 @@ def solution(operations):
         if command == 'I':
             heapq.heappush(min_heap, (num, i))
             heapq.heappush(max_heap, (-num, i))
-            visited[i] = True
+            
 
         elif command == 'D':
             if num == 1:
                 # 최댓값 삭제
-                while max_heap and not visited[max_heap[0][1]]:
+                while max_heap and visited[max_heap[0][1]]:
                     heapq.heappop(max_heap)
                 if max_heap:
-                    visited[max_heap[0][1]] = False
+                    visited[max_heap[0][1]] = 1
                     heapq.heappop(max_heap)
             else:
                 # 최솟값 삭제
-                while min_heap and not visited[min_heap[0][1]]:
+                while min_heap and visited[min_heap[0][1]]:
                     heapq.heappop(min_heap)
                 if min_heap:
-                    visited[min_heap[0][1]] = False
+                    visited[min_heap[0][1]] = 1
                     heapq.heappop(min_heap)
 
     # 최종 결과 정리
-    while min_heap and not visited[min_heap[0][1]]:
+    while min_heap and  visited[min_heap[0][1]]:
         heapq.heappop(min_heap)
-    while max_heap and not visited[max_heap[0][1]]:
+    while max_heap and  visited[max_heap[0][1]]:
         heapq.heappop(max_heap)
 
     if not min_heap or not max_heap:
