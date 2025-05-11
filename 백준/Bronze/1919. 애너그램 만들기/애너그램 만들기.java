e2c6acd889ddc88a152a76a64cfb7cc7dfd84066
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-05-11 23:03:09 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 172 ms, Memory: 17512 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int[] getAlphabetCount(String str){
+        int [] count = new int[26];
+
+        for(int i=0;i<str.length();i++){
+            count[str.charAt(i)-'a']++;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
-        int [] countA = new int[26];
-        int [] countB = new int[26];
-
-        for(int i=0;i<a.length();i++){
-            countA[a.charAt(i)-'a']++;
-        }
-        for(int i=0;i<b.length();i++){
-            countB[b.charAt(i)-'a']++;
-        }
+        int [] countA = getAlphabetCount(a);
+        int [] countB = getAlphabetCount(b);
+        
         int answer = 0;
         for(int i=0;i<26;i++){
-            if (countA[i] > countB[i]){
-                answer += countA[i] - countB[i];
-            } else if (countA[i] < countB[i]) {
-                answer += countB[i]- countA[i];
-            }
+            answer += Math.abs(countA[i] - countB[i]);
         }
         System.out.print(answer);
 
