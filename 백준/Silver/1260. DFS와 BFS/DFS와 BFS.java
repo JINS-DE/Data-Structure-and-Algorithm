5a65dd86060c140209eb0fc6f791720b922388fd
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-05-14 20:52:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 508 ms, Memory: 43272 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,59 @@
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+    static int n,m,v;
+    static int[][] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        v = sc.nextInt();
+        graph = new int[n+1][n+1];
+        visited = new boolean[n+1];
+        for(int i=0;i<m;i++){
+            int src = sc.nextInt();
+            int dst = sc.nextInt();
+            graph[src][dst]=1;
+            graph[dst][src]=1;
+        }
+        dfs(v);
+        System.out.println();
+        visited = new boolean[n+1];
+        bfs(v);
+
+    }
+    static void dfs(int node){
+        System.out.print(node + " ");
+        visited[node] = true;
+        for(int i=1;i<=n;i++){
+            if(graph[node][i]==1 && !visited[i]){
+                dfs(i);
+            }
+        }
+    }
+
+    static Queue<Integer> q;
+    static void bfs(int node){
+        q = new LinkedList<>();
+        q.add(node);
+        visited[node]=true;
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+            System.out.print(now + " ");
+            for(int i=1; i<=n;i++){
+                if(graph[now][i]==1 && !visited[i]){
+                    q.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
