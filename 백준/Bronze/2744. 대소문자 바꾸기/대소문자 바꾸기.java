5881a0623629857a35f55b42f790a3edba37d033
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-06-18 05:17:01 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        for (int i=0;i<str.length();i++){
-            char ch = str.charAt(i);
-            if ('A' <= ch && ch<='Z')
-                System.out.print((char)('a'+ch-'A'));
-            else
-                System.out.print((char)('A'+ch-'a'));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String word = br.readLine();
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            if (Character.isUpperCase(w)) {
+                answer.append(Character.toLowerCase(w));
+            } else {
+                answer.append(Character.toUpperCase(w));
+            }
         }
-
-    }
-}
+        System.out.println(answer);
+    }}
