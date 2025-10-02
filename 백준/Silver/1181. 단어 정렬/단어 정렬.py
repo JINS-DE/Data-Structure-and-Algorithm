acec19aac753c5109cdee441eb334d2bb0ed8c7e
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-02 22:23:58 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,20 +1,14 @@
 import sys
-n = int(input())
-words = []
+input = sys.stdin.readline
+
+n=int(input())
+sets = set()
 for _ in range(n):
-    word = sys.stdin.readline()
-    if word not in words:
-        words.append(word)
-arr = []
-for i in range(len(words)):
-    max_word = words[0]
-    index = 0
-    for j in range(1,len(words)-i):
-        if (len(words[j]) == len(max_word) and words[j] > max_word) or len(words[j]) > len(max_word):
-            max_word = words[j]
-            index = j
-    tmp = words[len(words)-i-1]
-    words[len(words)-i-1] = words[index]
-    words[index] = tmp
-for word in words:
-    sys.stdout.write(word)
+    alpha = input().strip()
+    sets.add(alpha)
+    
+alpha_li = list(sets)
+alpha_li.sort(key = lambda x : (len(x),x) )
+
+for st in alpha_li:
+    print(st)
