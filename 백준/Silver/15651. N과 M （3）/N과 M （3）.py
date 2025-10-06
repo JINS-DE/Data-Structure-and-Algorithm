268cd53904da0a31166d095c95d33145ce6de32c
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-06 18:44:39 +0900

    [Silver III] Title: N과 M (3), Time: 760 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,16 +1,14 @@
-N,M = map(int,input().split())
 
-answer=[]
-def dfs(depth,lst):
+import sys
+input = sys.stdin.readline
+N, M = map(int,input().split())
+
+def dfs(depth,st):
     if depth==M:
-        answer.append(lst)
+        print(" ".join(st))
         return
+    
     for i in range(1,N+1):
-        dfs(depth+1,lst+[i])
+        dfs(depth+1,st+f"{i}")
 
-
-        
-dfs(0,[])
-
-for i in answer:
-    print(*i)
+dfs(0,"")
