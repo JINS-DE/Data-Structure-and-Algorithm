58b5e3a9d464363b316c599f81d5cc94f7afd65a
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-06 18:21:46 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int,input().split())
-
-def dfs(depth,idx,lst):
-    if depth==M:
-        print(" ".join(map(str,lst)))
+def dfs(n,st):
+    if n>N:
+        if len(st)==M:
+            print(" ".join(st))
         return
-    
-    for i in range(idx,N+1):
-        dfs(depth+1,i+1,lst+[i])
-    
-dfs(0,1,[])
+    dfs(n+1,st+f"{n}")
+    dfs(n+1,st)
+
+dfs(1,"")
