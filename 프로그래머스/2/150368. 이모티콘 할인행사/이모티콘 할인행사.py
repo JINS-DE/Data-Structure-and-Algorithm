a6a3bc642ed13399bc7ca1b5a9fdfc001d094bf0
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-09 14:17:06 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2264.80 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,31 +1,40 @@
-def solution(users, emoticons):
-    n,m=len(users),len(emoticons)
-    discount=[0.9,0.8,0.7,0.6]
-    answer = [0,0]
-    def dfs(depth,tmp):
-        if depth==m:
-            plus=0
-            total_sum=0
-            for user_rate, plus_price in users:
-                tmp_sum=0
-                for emo_rate, emo_price in tmp:
-                    if user_rate<=emo_rate:
-                        tmp_sum+=emo_price                      
-                flag = tmp_sum // plus_price
-                if flag:
-                    plus+=1
-                else:
-                    total_sum += tmp_sum
-            if answer[0]<plus:
-                answer[0] = plus
-                answer[1] = total_sum
-            elif answer[0]==plus and answer[1] < total_sum:
-                answer[1]=total_sum
-                
-            return
-        for i in range(4):
-            dfs(depth+1,tmp+[[100-discount[i]*100, emoticons[depth]*discount[i]]])
+"""
+할인율은 10,20,30,40 중 하나
 
+"""
+def solution(users, emoticons):
+    discount_rates = [0.1,0.2,0.3,0.4] # 10%, 20%, 30%, 40%
+    result=[0,0]
+    def calc(lst):
+        cnt, price=0,0
+        tmp=0
+        for cell_line_rate, plus_price in users:
+            buy_total=0
+            for emoticon, discount_rate in lst:
+                if cell_line_rate <= discount_rate*100:
+                    buy_total+=emoticons[emoticon]*(1-discount_rate)
+            if plus_price <= buy_total:
+                cnt+=1
+            else:
+                price+=buy_total
+            tmp+=1
+        return cnt,price
     
+    def dfs(depth, lst):
+        if depth==len(emoticons):
+            register_cnt, cell_price = calc(lst)
+            if register_cnt > result[0]:
+                result[0] = register_cnt
+                result[1] = cell_price
+            elif register_cnt == result[0] and cell_price>result[1]:
+                result[1]=cell_price
+            return
+        
+        for rate in discount_rates:
+            dfs(depth+1,lst+[(depth,rate)])
+        
     dfs(0,[])
-    return answer
+    
+    
+    
+    return result
