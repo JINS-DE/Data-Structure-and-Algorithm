85f29a490bd8aac8b7cdcedb162e9c3d2a06640e
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-09 21:58:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 352.51 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,16 +1,17 @@
-import heapq
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    delivery = 0 
-    pickup = 0 
-    
-    for i in range(n-1, -1, -1): 
-        delivery += deliveries[i] 
-        pickup += pickups[i] 
+    delivery = 0
+    pickup = 0
+    # 맨 끝에서부터 있으면 무조건 다녀와야 됨.
+    # 맨 끝부터 loop를 돌면서 빚을 추가
+    for i in range(n-1,-1,-1):
+        delivery+=deliveries[i]
+        pickup+=pickups[i]
         
-        while delivery > 0 or pickup > 0: 
-            delivery -= cap 
-            pickup -= cap 
-            answer += (i+1) * 2 
-                 
+        # 빚이 생기면 빚을 다 갚을 때까지 loop
+        while delivery>0 or pickup>0:
+            delivery-=cap
+            pickup-=cap
+            answer+=(i+1)*2
+              
     return answer
