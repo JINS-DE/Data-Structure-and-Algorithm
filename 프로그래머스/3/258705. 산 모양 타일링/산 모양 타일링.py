30c669d36673e9f58b9ba44e998bf25e342fad10
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-14 14:26:13 +0900

    [level 3] Title: 산 모양 타일링, Time: 96.60 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,25 +1,25 @@
 """
 사다리꼴 안의 삼각형의 개수 = 2n+1 개
 윗변의 길이 n, 아랫변의 길이 n+1
+
+dp[n][0] : 오른쪽 끝이 삼각형으로 끝나는 경우
+dp[n][1] : 오른쪽 끝이 왼쪽으로긴 마름모로 끝나는 경우
 """
 def solution(n, tops):
+    # 1. 초기화
     mod = 10007
-    dp=[[0,0] for _ in range(n)]
-    
-    dp[0][0] = 2 if tops[0]==0 else 3
+    dp = [[0,0] for _ in range(n)]
+    dp[0][0] = 3 if tops[0]==1 else 2
     dp[0][1] = 1
     
+    # 2. memorization
     for i in range(1,n):
-        if tops[i]:
-            dp[i][0] = dp[i-1][0]*3 + dp[i-1][1] * 2   
+        if tops[i]==1:
+            dp[i][0] = dp[i-1][0]*3 + dp[i-1][1]*2
         else:
-            dp[i][0] = dp[i-1][0]*2 + dp[i-1][1]    
-            
-        dp[i][1] = dp[i-1][0] + dp[i-1][1]
+            dp[i][0] = dp[i-1][0]*2 + dp[i-1][1]
         
-        dp[i][0]%=mod
-        dp[i][1]%=mod
-    
-    answer = dp[n-1][0] + dp[n-1][1]
-    answer%=mod
-    return answer
+        dp[i][0] %= mod
+        dp[i][1] = (dp[i-1][1] + dp[i-1][0])%mod
+        
+    return (dp[n-1][0] + dp[n-1][1])%mod
