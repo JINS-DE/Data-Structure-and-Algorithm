35dce488c5265c64daf491f42e5343a65195cb74
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-14 16:53:08 +0900

    [Gold III] Title: 게임 개발, Time: 84 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -4,48 +4,37 @@ from collections import deque
 input = sys.stdin.readline
 N = int(input())
 
-# 1. 자료구조 초기화
+# 1. 초기화
 adj = [[] for _ in range(N + 1)]
 in_degree = [0] * (N + 1)
 build_time = [0] * (N + 1)
 result_time = [0] * (N + 1) # 각 건물이 최종 완성되는 시간
 
-# 2. 입력 받으며 그래프, 진입차수, 건설시간 완전하게 생성
-for i in range(1, N + 1):
-    line = list(map(int, input().split()))
-    build_time[i] = line[0]
-    
-    prerequisites = line[1:-1]
-    in_degree[i] = len(prerequisites)
-    
-    for pre in prerequisites:
-        adj[pre].append(i)
+# 2. 입력
+for i in range(N):
+    line = list(map(int,input().split()))
+    build_time[i+1]=line[0]
+    pre_node = line[1:-1]
+    in_degree[i+1] = len(pre_node)
+    for node in pre_node:
+        adj[node].append(i+1)
 
-# 3. 진입차수가 0인 노드를 큐에 추가 (위상정렬 시작점)
-q = deque()
-for i in range(1, N + 1):
-    if in_degree[i] == 0:
-        q.append(i)
-        result_time[i] = build_time[i] # 선수 건물이 없으므로 바로 건설 시작 및 완료
+# 3. 차수 0인애 q에 넣기
+q= deque()
+for node in range(1,N+1):
+    if in_degree[node]==0:
+        result_time[node]=build_time[node]
+        q.append(node)
 
-# 4. 위상 정렬 실행
+# 4. 위상정렬 
 while q:
-    current_node = q.popleft()
+    curr=q.popleft()
+    for nxt in adj[curr]:
+        in_degree[nxt]-=1
+        result_time[nxt] = max(result_time[nxt],result_time[curr]+build_time[nxt])
+        if in_degree[nxt]==0:
+            q.append(nxt)
 
-    for next_node in adj[current_node]:
-        # ★ 핵심 로직 ★
-        # next_node는 current_node가 끝난 후에야 지을 수 있으므로,
-        # next_node의 완료 시간은 최소한 current_node의 완료 시간만큼은 걸린다.
-        # 여러 선수 건물 중 가장 늦게 끝나는 시간을 저장해야 하므로 max 사용
-        result_time[next_node] = max(result_time[next_node], result_time[current_node])
-        
-        in_degree[next_node] -= 1
-
-        # 모든 선수 건물이 완성되었다면
-        if in_degree[next_node] == 0:
-            # 저장된 가장 늦은 완료 시간(시작 시간)에 자신의 건설 시간을 더한다
-            result_time[next_node] += build_time[next_node]
-            q.append(next_node)
 
 # 5. 결과 출력
 for i in range(1, N + 1):
