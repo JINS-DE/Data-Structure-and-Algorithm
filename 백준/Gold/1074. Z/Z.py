9f3bc2acf0b74457fd8d5ae36781ff9fb6b52101
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-20 21:42:28 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,22 +1,20 @@
-import sys
-sys.setrecursionlimit(1000000)
-N,r,c = map(int,sys.stdin.readline().split())
-
-def z(n, r, c):
-    if n == 0:
+N,r,c = map(int,input().split())
+def Z(n,r,c):
+    if n==0:
         return 0
-
-    half = 2 ** (n - 1)
-    if r < half and c < half:  # 왼쪽 위
-        return z(n - 1, r, c)
-    elif r < half and c >= half:  # 오른쪽 위
-        return half * half + z(n - 1, r, c - half)
-    elif r >= half and c < half:  # 왼쪽 아래
-        return 2 * half * half + z(n - 1, r - half, c)
-    else:  # 오른쪽 아래
-        return 3 * half * half + z(n - 1, r - half, c - half)
-
     
+    half = 2**(n-1)
+    # 1사분면
+    if r<half and c<half:
+        return Z(n-1,r,c)
+    # 2사분면
+    elif r<half and c>=half:
+        return half*half + Z(n-1,r,c-half)
+    # 3사분면
+    elif r>=half and c<half:
+        return 2*half*half + Z(n-1,r-half,c)
+    # 4사분면
+    else:
+        return 3*half*half + Z(n-1,r-half,c-half)
 
-ans = z(N,r,c)
-print(ans)
+print(Z(N,r,c))
