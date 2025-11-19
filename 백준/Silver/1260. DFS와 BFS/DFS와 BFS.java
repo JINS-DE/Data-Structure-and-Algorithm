ac3bf00678888296ac2fe40efdcd01f7b7d1d3c8
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-11-19 14:39:37 +0900

    [Silver II] Title: DFS와 BFS, Time: 504 ms, Memory: 39312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,57 +1,60 @@
 
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    static int n,m,v;
-    static int[][] graph;
+    static int N,M,V;
+    static List<List<Integer>> graph;
     static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        v = sc.nextInt();
-        graph = new int[n+1][n+1];
-        visited = new boolean[n+1];
-        for(int i=0;i<m;i++){
-            int src = sc.nextInt();
-            int dst = sc.nextInt();
-            graph[src][dst]=1;
-            graph[dst][src]=1;
+        N = sc.nextInt();
+        M = sc.nextInt();
+        V = sc.nextInt();
+        graph = new ArrayList<>();
+        for (int i=0;i<=N;i++){
+            graph.add(new ArrayList<>());
         }
-        dfs(v);
-        System.out.println();
-        visited = new boolean[n+1];
-        bfs(v);
 
+        for (int i=0;i<M;i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i=1;i<=N;i++){
+            Collections.sort(graph.get(i));
+        }
+        visited = new boolean[N+1];
+        dfs(V);
+        System.out.println();
+        visited = new boolean[N+1];
+        bfs(V);
     }
-    static void dfs(int node){
+    static void dfs(int node) {
         System.out.print(node + " ");
         visited[node] = true;
-        for(int i=1;i<=n;i++){
-            if(graph[node][i]==1 && !visited[i]){
-                dfs(i);
+        for (int nextNode : graph.get(node)) {
+            if (!visited[nextNode]) {
+                dfs(nextNode);
             }
         }
     }
-
     static Queue<Integer> q;
     static void bfs(int node){
         q = new LinkedList<>();
-        q.add(node);
-        visited[node]=true;
+        q.offer(node);
+        visited[node] = true;
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()){
             int now = q.poll();
-            System.out.print(now + " ");
-            for(int i=1; i<=n;i++){
-                if(graph[now][i]==1 && !visited[i]){
-                    q.offer(i);
-                    visited[i] = true;
+            System.out.print(now+" ");
+            for (int nextNode:graph.get(now)){
+                if (!visited[nextNode]){
+                    q.offer(nextNode);
+                    visited[nextNode]=true;
                 }
             }
         }
