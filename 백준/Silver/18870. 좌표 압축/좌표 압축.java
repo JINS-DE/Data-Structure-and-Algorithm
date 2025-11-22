4d70f0885576c32e05ccfc57b572bfe8cf8646e1
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-11-22 21:25:46 +0900

    [Silver II] Title: 좌표 압축, Time: 1776 ms, Memory: 344392 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -7,27 +7,25 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int [][] arr = new int[N][2];
+        Set<Integer> set = new TreeSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int i =0;
-        while(st.hasMoreTokens()){
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = i;
-            i++;
+        int[] arr = new int[N];
+        for (int i=0; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            set.add(num);
+            arr[i] = num;
         }
-        Arrays.sort(arr,(o1,o2)->o1[0]-o2[0]);
-        int[] answer = new int[N];
-        answer[arr[0][1]] = 0;
-        int pressIdx = 0;
-        for (int j=1; j<N;j++){
-            if (arr[j-1][0] != arr[j][0]) {
-                pressIdx++;
-            }
-            answer[arr[j][1]] = pressIdx;
+        Map<Integer,Integer> map = new HashMap<>();
+        int idx=0;
+        for (int num : set){
+            map.put(num,idx++);
         }
-        for (int j=0;j<N;j++){
-            bw.write(answer[j] +" ");
+
+
+        for (int i=0;i<N;i++){
+            bw.write(map.get(arr[i]) + " ");
         }
         bw.flush();
+
     }
 }
