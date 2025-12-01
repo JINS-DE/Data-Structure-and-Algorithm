ee729c5ba84079b9f09fa7c3f2a4a0a3d9e9d9fb
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-12-01 10:44:30 +0900

    [Gold V] Title: 수 고르기, Time: 248 ms, Memory: 25184 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.io.*;
 public class Main{
     static int N,M;
-    static Integer[] arr;
+    static int[] arr;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new Integer[N];
+        arr = new int[N];
         for (int i=0; i<N; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
