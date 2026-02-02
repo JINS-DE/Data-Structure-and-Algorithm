0cee0f9dc74a078c54695bf00e7e43bca60f4003
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-02-02 23:36:28 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,8 +1,21 @@
 class Solution {
     public long solution(long n) {
-        long root = (long) Math.sqrt(n);
-        if (root*root==n){
-            return (root+1)*(root+1);
+        long left = 1;
+        // 핵심 수정: n이 아무리 커도 제곱근은 3,037,000,499를 넘을 수 없습니다.
+        // 불필요한 큰 수 계산을 막아 오버플로우를 방지합니다.
+        long right = Math.min(n, 3037000499L); 
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long pow = mid * mid; 
+
+            if (pow == n) {
+                return (mid + 1) * (mid + 1);
+            } else if (pow < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
         return -1;
     }
