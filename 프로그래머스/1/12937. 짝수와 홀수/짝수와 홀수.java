15153396a77475912d1557b34b148d7c1fec1544
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-02-04 00:03:01 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(int num) {
-        
-        if (num%2==0){
-            return "Even"; // 짝수
-        } else{
-            return "Odd"; // 홀수
-        }
-        
+       return (num%2==0) ? "Even" : "Odd";
     }
 }
