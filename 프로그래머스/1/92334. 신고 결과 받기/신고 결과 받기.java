e4852f91c803189ef3c6f2d775ce718c568c7281
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-02-26 08:47:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 164.63 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,9 +4,6 @@ HashMap<String,Integer> reportedCount = new HashMap<>();
 
 내가 신고한 사람들 목록 관리
 HashMap<String,Set<String>> reportList = new HashMap<>();
-
-정지당한유저 목록
-List<String> stopUserList = new ArrayList<>();
 */
 
 import java.util.*;
@@ -15,47 +12,42 @@ class Solution {
         // 0. 초기화 
         int n = id_list.length;
         int[] answer = new int[n];
-        HashMap<String,Integer> reportedCount = new HashMap<>();
-        HashMap<String,Set<String>> reportList = new HashMap<>();
-        List<String> stopUserList = new ArrayList<>();
-        // 1. report 순회, reportedCount, reportList 주입
-        for (String reportUser : report){
-            String[] user = reportUser.split(" ");
-            String reporter = user[0];
-            String reported = user[1];
+        Map<String,Set<String>> reportList = new HashMap<>();
+        Map<String,Integer> reportedCount = new HashMap<>();
+        
+        // 1. reportList, reportedCount 데이터 주입
+        for (String reportInfo : report){
+            String[] tmp = reportInfo.split(" ");
+            String reporter = tmp[0];
+            String reported = tmp[1];
             
-            // 내가 신고한 목록 reportList
-            var set = reportList.get(reporter);
-            if (set==null) {
-                set = new HashSet<>();
-                reportList.put(reporter,set);
-                };
-            
-            if (!reportList.get(reporter).contains(reported)){
-                reportedCount.put(reported,reportedCount.getOrDefault(reported,0)+1);    
+            // 코드 개선할 곳
+            Set<String> reportedUserList = reportList.get(reporter);
+            if ( reportedUserList == null){
+                reportedUserList = new HashSet<>();
+                reportList.put(reporter, reportedUserList);
             }
-                    
-            set.add(reported);
-            // reportList.computeIfAbsent(reporter, k -> new HashSet<>()).add(reported);
-        }
-        // 2. id_list 순회, reportedCount 체크, 신고 당한 횟수 k 이상인 stopUserList 추가 
-        for (int i=0; i<n; i++) {
-            String user = id_list[i];
-            if (reportedCount.getOrDefault(user,0) >= k){
-                stopUserList.add(user);
+            // 중복 신고 시 reportedCount의 count는 1회 처리 
+            if ( !reportedUserList.contains(reported) ){
+                reportedUserList.add(reported);
+                reportedCount.put(reported,reportedCount.getOrDefault(reported,0)+1);
             }
         }
         
-        // 3. id_list 순회 stopUserList count 세주고 answer에 count 추가
-        for (int i=0; i<n; i++){
+        // 2. reportList 순회하면서 k 값이 넘는 사람이 있으면 answer 추가
+        for (int i=0; i<n;i++){
             String user = id_list[i];
-            Set<String> set = reportList.get(user);
-            if (set==null) continue;
-            int count = 0;
-            for (String stopUser:stopUserList){
-                if (set.contains(stopUser)) count++;
+            int mailSendCount=0;
+            var sets = reportList.get(user);
+            if (sets == null) continue;
+            for (String reportedUser : sets){
+                Integer count = reportedCount.get(reportedUser);
+                if (count!=null && count>=k){
+                    mailSendCount++;
+                }
+                
             }
-            answer[i] = count;
+            answer[i] = mailSendCount;
         }
         
         return answer;
