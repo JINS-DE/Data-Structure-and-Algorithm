830c8c030f8cff4171566c39fbddfd23eccd5d45
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-02-26 10:05:12 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 20.71 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,58 +1,67 @@
-/* 문제 풀이
-1. friends 총 개수 = n 이라면 n*n 2차원 배열 만들기 -> 선물 개수 갱신 
-2. friends의 각 index 별로 hash 저장 (ex)"muzi":0, "ryan":1 
-3. 선물 지수용 hash -> 선물 받으면 -1 주면 +1
-4. gifts 돌면서 "선물개수","선물지수" 갱신
-5. 2중 for문을 돌면서 각각 받을 선물 개수 구하면서 max값 갱신하기. Default max=0
+/*
+- A가 B에게, B가 A에게 선물 몇번 줬는지 저장소 (내가 선물 준 사람의 리스트)
+Map<String,Map<String,Integer>> presentList
+(ex) { A:{B:3, C:4}, B:{A:4, C:3} }
+- 선물지수 기록 (이번달 준 선물의 수 - 받은 선물의 수)
+int[] presentDegree;
 */
-
 import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int n = friends.length;
-        // 1번
-        int[][] presentCountArray = new int[n][n];
-        
-        // 2번
-        Map<String,Integer> friendIndex = new HashMap<>();
-        for (int i=0;i<n;i++){
-           friendIndex.put(friends[i],i);
+        // 0. 초기화
+        int[] answer = new int[friends.length];
+        int[] presentDegree = new int[friends.length];
+        Map<String,Map<String,Integer>> presentList = new HashMap<>();
+        Map<String,Integer> friendIdx = new HashMap<>();
+        for (int i=0; i< friends.length; i++){
+            friendIdx.put(friends[i], i);
         }
         
-        // 3번
-        Map<String,Integer> friendsDegree = new HashMap<>();
-        
-        // 4번
-        for (int i=0; i<gifts.length;i++){
-            String[] tmp = gifts[i].split(" ");
-            String giver = tmp[0];
-            String given = tmp[1];
-            presentCountArray[friendIndex.get(giver)][friendIndex.get(given)]+=1;
-            friendsDegree.put(giver,friendsDegree.getOrDefault(giver,0)+1);
-            friendsDegree.put(given,friendsDegree.getOrDefault(given,0)-1);
+        // 1. 데이터 입력
+        for (String gift:gifts){
+            String[] users = gift.split(" ");
+            String giver = users[0];
+            String receiver = users[1];
+            
+            // 1-1. presentList 추가
+            // Map<String,Map<String,Integer>> presentList
+            if (!presentList.containsKey(giver)){
+                presentList.put(giver,new HashMap<>());
+            }
+            var receiverList = presentList.get(giver);
+            receiverList.put(receiver,receiverList.getOrDefault(receiver, 0)+1);
+            
+            // 1-2. presentDegree 업데이트
+            presentDegree[friendIdx.get(giver)]++;
+            presentDegree[friendIdx.get(receiver)]--;
         }
-        int[] answer = new int[n];
-        // 5번
-        for (int i=0; i<n-1;i++){
-            for(int j=i+1; j<n;j++){
-                int tmp;
-                if (presentCountArray[i][j] > presentCountArray[j][i]){
-                    answer[i]+=1;
-                }else if((presentCountArray[i][j] < presentCountArray[j][i])){
-                    answer[j]+=1;
-                }else{
-                    int degreeI = friendsDegree.getOrDefault(friends[i],0);
-                    int degreeJ = friendsDegree.getOrDefault(friends[j],0);
-                    if (degreeI > degreeJ){
-                        answer[i]+=1;
-                    } else if(degreeI<degreeJ){
-                        answer[j]+=1;
-                    }
-                    
+        
+        
+        // 2. friends 순회, 받을 선물의 수 갱신(max(answer,cnt))
+        
+        // { A:{B:3, C:4}, B:{A:4, C:3} }
+        for (int i=0; i<friends.length-1;i++){
+            String a = friends[i];
+            for (int j=i+1; j<friends.length;j++){
+                String b = friends[j];
+                int aScore = presentList.getOrDefault(a,new HashMap<>()).getOrDefault(b,0);
+                int bScore = presentList.getOrDefault(b,new HashMap<>()).getOrDefault(a,0);
+                int aIdx = friendIdx.get(a);
+                int bIdx = friendIdx.get(b);
+                if (aScore > bScore){
+                    answer[aIdx] ++;
+                } else if (bScore> aScore){
+                    answer[bIdx] ++;
+                } else{
+                    if (presentDegree[aIdx] > presentDegree[bIdx]){
+                        answer[aIdx] ++;
+                    } else if (presentDegree[aIdx] < presentDegree[bIdx]){
+                        answer[bIdx] ++;
+                    } 
                 }
             }
+            
         }
-        
-        return Arrays.stream(answer).max().getAsInt();
+        return Arrays.stream(answer).max().orElse(0);
     }
 }
