dbca755749d808efbe15a7617160c447e4a63650
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-03-23 23:41:46 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -2,44 +2,48 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static String st;
-    public static void main(String[] args) throws IOException {
+    static String alpha;
+    public static void main(String[] args) throws IOException{
         input();
-        int N = st.length();
-        int answer = 0;
-        for (int i=0; i<N;i++){
-            if (i==N-1) {
-                answer ++;
-                break;
-            }
-            char ch = st.charAt(i);
-            char nextChar = st.charAt(i+1);
-            if (ch == 'c'){
-                if ( nextChar == '=' || nextChar == '-'){
-                    i ++;
-                }
-            } else if( ch == 'd'){
-                if ( nextChar == '-'){
+        int N = alpha.length();
+        int answer = N;
+        for (int i=0; i<N-1;i++){
+            char c = alpha.charAt(i);
+            char next = alpha.charAt(i+1);
+
+            if (c=='s' || c=='z'){
+                if ( next == '='){
+                    answer --;
                     i++;
-                } else if (i+2<N && nextChar == 'z' && st.charAt(i+2) == '='){
+                }
+            } else if (c=='c'){
+                if (next == '=' || next=='-'){
+                    answer--;
+                    i++;
+                }
+            } else if (c=='l' || c=='n'){
+                if ( next == 'j'){
+                    answer --;
+                    i++;
+                }
+            } else if (c=='d') {
+                if (next == '-') {
+                    answer--;
+                    i++;
+                } else if (i+2<N && next == 'z' && alpha.charAt(i+2)=='=') {
+                    answer -=2;
                     i+=2;
                 }
-            } else if( ch == 'l' && nextChar == 'j'){
-                i++;
-            } else if( ch == 'n' && nextChar == 'j' ){
-                i++;
-            } else if( ch == 's' && nextChar =='=') {
-                i++;
-            } else if( ch == 'z' && nextChar =='=') {
-                i++;
+
             }
-            answer ++;
         }
         System.out.println(answer);
     }
 
+
+
     static void input() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = br.readLine();
+        alpha = br.readLine();
     }
 }
