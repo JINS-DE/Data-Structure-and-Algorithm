38e7d5b661c492bc7fc8ba0c41a71c11415b4215
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-03-23 23:44:37 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -5,39 +5,11 @@ public class Main {
     static String alpha;
     public static void main(String[] args) throws IOException{
         input();
-        int N = alpha.length();
-        int answer = N;
-        for (int i=0; i<N-1;i++){
-            char c = alpha.charAt(i);
-            char next = alpha.charAt(i+1);
-
-            if (c=='s' || c=='z'){
-                if ( next == '='){
-                    answer --;
-                    i++;
-                }
-            } else if (c=='c'){
-                if (next == '=' || next=='-'){
-                    answer--;
-                    i++;
-                }
-            } else if (c=='l' || c=='n'){
-                if ( next == 'j'){
-                    answer --;
-                    i++;
-                }
-            } else if (c=='d') {
-                if (next == '-') {
-                    answer--;
-                    i++;
-                } else if (i+2<N && next == 'z' && alpha.charAt(i+2)=='=') {
-                    answer -=2;
-                    i+=2;
-                }
-
-            }
+        String[] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        for (String st : arr){
+            alpha = alpha.replace(st,"!");
         }
-        System.out.println(answer);
+        System.out.println(alpha.length());
     }
 
 
