8dbea65a78109738dc14107b70ec389efaa93d3e
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-03-27 23:30:55 +0900

    [level 2] Title: 올바른 괄호, Time: 5.34 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 54.4 MB, 시간: 6.71 ms
+메모리: 54.4 MB, 시간: 5.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 18:48:03
+2026년 03월 27일 23:30:51
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,30 +1,17 @@
-/*
-그냥 스택 포인트를 사용하면 될듯?
-stackPoint -> -1 이면 빈 스택
-(가 들어오면 +1, 
-)가 들어오면 stackPoint가 -1인지 확인(스택이 비어있나확인)하고 -1 비어있다면 false
-
-
-*/
+import java.util.*;
 class Solution {
     boolean solution(String s) {
-        int stackPoint = -1;
-        for (int i=0;i<s.length();i++){
-          if (s.charAt(i) == '('){
-              stackPoint+=1;
-          } else{
-              if (stackPoint<0){
-                  return false;
-              } else{
-                  stackPoint-=1;
-              }
-          }
+        int count = 0;
+        for (int i=0; i<s.length();i++){
+            char c = s.charAt(i);
+            if (c=='('){
+                count++;
+            } else{
+                if (count <= 0 ) return false;
+                count--;
+            }
         }
-        if (stackPoint==-1){
-            return true;
-        }else{
-            return false;
-        }
-        
+        if (count>0) return false;
+        return true;
     }
 }
