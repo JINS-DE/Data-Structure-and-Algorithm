69880f08637e85726bb5068723f82a2c131774b4
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-03-31 23:43:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.96 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,20 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String,Integer> hash = new HashMap<>();
-        for (String p : participant){
-            hash.put(p,hash.getOrDefault(p,0) + 1);
+        Map<String, Integer> hm = new HashMap<>();
+
+        // 1. 모든 참가자 카운팅 (+)
+        for (String p : participant) hm.put(p, hm.getOrDefault(p, 0) + 1);
+        
+        // 2. 완주자 카운팅 (-)
+        for (String c : completion) hm.put(c, hm.get(c) - 1);
+
+        // 3. 0이 아닌 (남은 1명) 사람 찾기
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0) return key;
         }
-        for (String c : completion){
-            int cnt = hash.get(c);
-            cnt --;
-            if (cnt<=0){
-                hash.remove(c);
-            } else{
-                hash.put(c,cnt);
-            }
-        }
-        String answer="";
-        for (String s : hash.keySet()){
-            answer = s;
-        }
-        return answer;
+
+        return "";
     }
 }
