aef9dcfdc4a50976c38ea012ec13b236c128a460
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-05-06 07:29:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.32 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?utm_source=chatgpt.com) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.8 MB, 시간: 83.96 ms
+메모리: 98.1 MB, 시간: 84.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 31일 23:43:06
+2026년 05월 06일 07:29:12
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,22 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> hm = new HashMap<>();
-
-        // 1. 모든 참가자 카운팅 (+)
-        for (String p : participant) hm.put(p, hm.getOrDefault(p, 0) + 1);
-        
-        // 2. 완주자 카운팅 (-)
-        for (String c : completion) hm.put(c, hm.get(c) - 1);
-
-        // 3. 0이 아닌 (남은 1명) 사람 찾기
-        for (String key : hm.keySet()) {
-            if (hm.get(key) != 0) return key;
+        // 1. 해쉬 "이름" : "나온 횟수"
+        Map<String,Integer> hash = new HashMap<>();
+        for (String name : participant){
+            hash.put(name, hash.getOrDefault(name,0)+1);
         }
-
+        // 2. completion 순회, 해당 해쉬 count -- 
+        for (String name : completion){
+            hash.put(name,hash.get(name)-1);
+        }
+        
+        // 3. count > 0 return
+        for (String name : hash.keySet()){
+            if (hash.get(name)>0){
+                return name;
+            }
+        }
         return "";
     }
 }
