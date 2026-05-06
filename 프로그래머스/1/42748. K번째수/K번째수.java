0f847b14af53da08c17f071154285e9191ba8a1a
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-05-07 08:39:11 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,35 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        int idx = 0;
+        int n = 0;
         for (int[] command : commands){
-            int[] arr = Arrays.copyOfRange(array, command[0]-1, command[1]); 
+            n ++;
+        } 
+        int[] answer = new int[n];
+        for (int i=0; i<n; i++){
+            int[] command = commands[i];
+            int s = command[0];
+            int e = command[1];
+            int k = command[2];
+            // 1. array 자른 배열 복사
+            int[] arr = copyArray(array,s,e);
+        
+            // 2. 자른 배열 정렬
             Arrays.sort(arr);
-            answer[idx] = arr[command[2]-1]; 
+        
+            // 3. k번째 수 구하기    
+            answer[i] = arr[k-1];
+        }
+        
+        return answer;
+    }
+    private int[] copyArray(int[] arr, int s, int e){
+        int[] newArr = new int[e-s+1];
+        int idx = 0;
+        for (int i = s-1 ; i<e; i++){
+            newArr[idx] = arr[i];
             idx++;
         }
-        return answer;
+        return newArr;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 78.1 MB, 시간: 0.49 ms
+메모리: 89.9 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 31일 19:19:49
+2026년 05월 07일 08:39:08
 
 ### 문제 설명
 
