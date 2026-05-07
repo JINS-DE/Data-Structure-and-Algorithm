3a64d1c34fba321eba51979ac8f8bedf72a303ee
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-05-08 08:36:09 +0900

    [level 2] Title: 전화번호 목록, Time: 68.78 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 217 MB, 시간: 289.28 ms
+메모리: 114 MB, 시간: 68.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 19일 23:38:08
+2026년 05월 08일 08:36:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,25 @@
 import java.util.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        Collections.addAll(set, phone_book);
-
-        for (String number : phone_book) {
-            for (int i = 1; i < number.length(); i++) {
-                if (set.contains(number.substring(0, i))) {
-                    return false;
+        Set<String> prefix = new HashSet<>();
+        Set<Integer> sizeList = new HashSet<>();
+        Arrays.sort(phone_book, (o1,o2)->o1.length()-o2.length());
+        
+        for (String num : phone_book){
+            if (prefix.contains(num)){
+                return false;
+            } else{
+                
+                
+                for (int size : sizeList){
+                   if (prefix.contains(num.substring(0,size))) return false;
                 }
+                
+                
+                prefix.add(num);
+                sizeList.add(num.length());
             }
         }
-
         return true;
     }
 }
