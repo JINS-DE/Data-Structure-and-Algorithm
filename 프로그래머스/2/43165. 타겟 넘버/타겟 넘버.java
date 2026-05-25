8a92ff940d66543a195ac2525b3e5f4a7d869384
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-05-25 12:47:47 +0900

    [level 2] Title: 타겟 넘버, Time: 2.84 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers, int target) {
-        return count(numbers,target,0,0,0);
+        return count(numbers,target,0,0);
     }
     
-    private int count(int[] numbers, int target, int idx, int sum, int answer){
+    private int count(int[] numbers, int target, int idx, int sum){
         if (idx>=numbers.length){
-            if (sum==target) return 1;
-            return 0;
+            return sum==target ? 1 : 0;
         }
-        return count(numbers,target,idx+1,sum+numbers[idx], answer) + count(numbers,target,idx+1,sum-numbers[idx], answer);
+        int plus = count(numbers,target,idx+1,sum+numbers[idx] );
+        int minus = count(numbers,target,idx+1,sum-numbers[idx]);
+        return plus+minus;
     }
 }
