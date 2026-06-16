bb3527e37f331fa0aa2ddb691457022c83ad986f
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-06-17 07:48:39 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 0.05 ms
+메모리: 74.7 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 16일 08:27:45
+2026년 06월 17일 07:48:36
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,6 +1,13 @@
 /*
-brown = width*2 + height*2 -4
-height = (brown-width*2+4)/2
+
+totalCells = brown + yellow
+width = totalCells/height
+
+# height의 최대값 
+height * width = totalCells
+height*height <= height*width = totalCells (가로가 세로보다 무조건 크다는 조건이 있음)
+height^2 <= totalCells
+height <= 루트 totalCells 성립
 
 yellow = (width-2) * (height-2)
 
@@ -8,18 +15,15 @@ yellow = (width-2) * (height-2)
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int width = 0;
-        int height = 0;
-        for (int wid = 3 ; wid<brown; wid++){
-            int hei = (brown-wid*2+4)/2;
-            int total = (wid-2) * (hei-2);
-            if (total == yellow){
-                width = wid;
-                height = hei;
-                break;
+        int totalCells = brown + yellow;
+        
+        for (int height = 3; height <= Math.sqrt(totalCells); height++){
+            int width = totalCells / height;
+            
+            if( (width-2)*(height-2) == yellow){
+                return new int[]{width,height};
             }
         }
-        return new int[]{Math.max(width,height),Math.min(width,height)};
-        // return new int[]{1,1};
+        return new int[0];
     }
 }
