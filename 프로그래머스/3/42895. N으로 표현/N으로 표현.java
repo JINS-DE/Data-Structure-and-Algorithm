9331e78f41f5dfdaff31c29c5b0489aecdf54133
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-07-09 09:24:03 +0900

    [level 3] Title: N으로 표현, Time: 18.87 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,45 +1,60 @@
+/*
+# cnt=1
+5
+
+# cnt = 2
+55, 5+5, 5-5, 5*5, 5/5
+-> NN + (cnt1 op cnt1)
+
+# cnt = 3
+-> NNN + (cnt1 op cnt2) + (cnt2 op cnt1)
+
+# cnt = 4
+-> NNNN + (cnt1 op cnt3) + (cnt2 op cnt2) + (cnt3 op cnt1)
+*/
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        if (N==number){
-            return 1;
+        Set<Integer>[] sets = new Set[9];
+        for (int i=1;i<=8;i++){
+            sets[i] = new HashSet<>();
         }
         
-        Set<Integer>[] dp = new Set[9];
-        for (int i=1;i<9;i++){
-            dp[i] = new HashSet<>();
-        }
-        
-        for (int i=1;i<9;i++){
-            dp[i].add(getSequnceNumber(N,i));
-            for (int j=1; j<i; j++){
-                Set<Integer> a = dp[j];
-                Set<Integer> b = dp[i-j];
+        for (int cnt=1; cnt<=8; cnt++){
+            sets[cnt].add(getSequenceNumber(N,cnt));
+            for (int i=1; i<cnt; i++){
+                Set<Integer> a = sets[i];
+                Set<Integer> b = sets[cnt-i];
                 
-                for (int num1 : a){
-                    for (int num2 : b){
-                        dp[i].add(num1+num2);
-                        dp[i].add(num1-num2);
-                        dp[i].add(num1*num2);
-                        if (num2!=0){
-                            dp[i].add(num1/num2);
+                for (int n1 : a){
+                    for (int n2 : b){
+                        sets[cnt].add(n1+n2);
+                        sets[cnt].add(n1-n2);
+                        sets[cnt].add(n1*n2);
+                        if (n2!=0){
+                            sets[cnt].add(n1/n2);    
                         }
                     }
                 }
             }
-            if (dp[i].contains(number)){
-                return i;
+            if (sets[cnt].contains(number)){
+                return cnt;
             }
         }
+        
         
         return -1;
     }
     
-    private int getSequnceNumber(int N, int count){
-        int result = 0;
-        for (int i=0;i<count;i++){
-            result = result*10 + N;
+    private int getSequenceNumber(int num,int cnt){
+        // cnt = 1 -> 5
+        // cnt = 2 -> 55
+        // cnt = 3 -> 555
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<cnt;i++){
+            sb.append(num);
         }
-        return result;
+        return Integer.parseInt(sb.toString());
     }
 }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 81.9 MB, 시간: 12.52 ms
+메모리: 81 MB, 시간: 18.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 08일 12:43:20
+2026년 07월 09일 09:24:00
 
 ### 문제 설명
 
