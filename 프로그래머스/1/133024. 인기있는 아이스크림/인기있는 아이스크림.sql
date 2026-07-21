8a4c566b1b51e82480d0ccb3bff0b5162371ea0d
Deon <115610267+JINS-DE@users.noreply.github.com>
2026-07-21 10:21:16 +0900

    [level 1] Title: 인기있는 아이스크림, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -0,0 +1,11 @@
+SELECT FLAVOR FROM FIRST_HALF ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC;
+
+/*
+104	caramel	2600
+101	chocolate	3200
+103	mint_chocolate	1700
+106	peach	2450
+109	strawberry	3100
+102	vanilla	2800
+105	white_chocolate	3100
+*/
